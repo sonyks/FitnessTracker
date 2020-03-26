481325cd07a983138024af083d7a6effbd6f235c
Oleksandr Poliukhovych <O.Bezliudnova@wolterskluwer.com>
2020-03-26 02:21:33 +0200

    added angularfire2 to project

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { TrainingService } from '../training.service';
 import { Exercise } from '../exercise.model';
 import { NgForm } from '@angular/forms';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { Subscription, Observable } from 'rxjs';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-new-training',
@@ -10,13 +13,22 @@ import { NgForm } from '@angular/forms';
 })
 export class NewTrainingComponent implements OnInit {
 
-  exercises: Exercise[] = [
-
-  ];
-  constructor(private trainingService: TrainingService) { }
-
+  exercises: Observable<Exercise[]>;
+  constructor(private trainingService: TrainingService,
+    private angularFirestore: AngularFirestore) { }
+  
   ngOnInit(): void {
-    this.exercises = this.trainingService.getAvailableExercises();
+    this.exercises = this.angularFirestore
+    .collection('availableExercises')
+    .snapshotChanges()
+    .map(docData => {
+      return docData.map(doc => {
+        return {
+          id: doc.payload.doc.id,
+          ...doc.payload.doc.data() as Exercise
+        }
+      })
+    });
   }
 
   onStartTraining(form: NgForm) {
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyB04sVz3EsNdfCKE_QoIN3i64huVQE_Q2k",
+    authDomain: "fitness-tracker-b6795.firebaseapp.com",
+    databaseURL: "https://fitness-tracker-b6795.firebaseio.com",
+    projectId: "fitness-tracker-b6795",
+    storageBucket: "fitness-tracker-b6795.appspot.com",
+    messagingSenderId: "848383015129",
+    appId: "1:848383015129:web:a94c053acc4e49b74ca6c8",
+    measurementId: "G-MMFBZH096Z"
+  }
 };
 
 /*
